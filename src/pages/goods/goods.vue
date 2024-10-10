43331d96380af66672c11d7397b9d54b53a08d94
menglongxiang <embermeng@gmail.com>
2024-10-10 16:58:01 +0800

    feat: SKU模块-加入购物车

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -7,9 +7,11 @@ import type { GoodsResult } from '@/types/goods'
 import AddressPanel from './components/AddressPanel.vue'
 import ServicePanel from './components/ServicePanel.vue'
 import type {
+    SkuPopupEvent,
     SkuPopupInstance,
     SkuPopupLocaldata,
 } from '@/components/vk-data-goods-sku-popup/vk-data-goods-sku-popup'
+import { postMemberCartApi } from '@/services/cart'
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
@@ -104,6 +106,12 @@ const skuPopupRef = ref<SkuPopupInstance>()
 const selectArrText = computed(() => {
     return skuPopupRef.value?.selectArr?.join(' ').trim() || '请选择商品规格'
 })
+// 加入购物车事件
+const onAddCart = async (ev: SkuPopupEvent) => {
+    await postMemberCartApi({ skuId: ev._id, count: ev.buy_num })
+    uni.showToast({ title: '添加成功', icon: 'none' })
+    isShowSku.value = false
+}
 </script>
 
 <template>
@@ -116,6 +124,7 @@ const selectArrText = computed(() => {
         buy-now-background-color="#27BA9B"
         :actived-style="{ color: '#27BA9B', borderColor: '#27BA9B', backgroundColor: '#E9F8F5' }"
         ref="skuPopupRef"
+        @add-cart="onAddCart"
     />
     <scroll-view scroll-y class="viewport">
         <!-- 基本信息 -->
